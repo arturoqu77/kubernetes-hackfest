473a20e8936c8ed2dac54f308d500654b6f94c58
Arturo R. Quiroga <arturoqu@hotmail.ca>
2020-12-20 10:44:25 -0500

    image size

--- labs/build-application/build-containers.sh
+++ labs/build-application/build-containers.sh
@@ -1,6 +1,9 @@
 #
-# az acr build -t hackfest/data-api:1.0 -r $ACRNAME --no-logs -o json app/data-api
+az acr build -t hackfest/data-api:1.0 -r $ACRNAME --no-logs -o json app/data-api
 az acr build -t hackfest/flights-api:1.0 -r $ACRNAME --no-logs -o json app/flights-api
 az acr build -t hackfest/quakes-api:1.0 -r $ACRNAME --no-logs -o json app/quakes-api
 az acr build -t hackfest/weather-api:1.0 -r $ACRNAME --no-logs -o json app/weather-api
 az acr build -t hackfest/service-tracker-ui:1.0 -r $ACRNAME --no-logs -o json app/service-tracker-ui
+
+# to show size of an image in the ACR
+az acr repository show-manifests -n $ACRNAME --repository hackfest/data-api --detail --query '[].{Size: imageSize, Tags: tags[0],Created: createdTime, Architecture: architecture, OS: os}' -o table
