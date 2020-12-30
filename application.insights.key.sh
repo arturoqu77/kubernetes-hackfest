d3ec2fca4d36d95b3cd18a99c332978f2c84b583
arturoqu77 <arturoqu@hotmail.ca>
2020-12-30 14:57:21 -0500

    modified values

---
+++ application.insights.key.sh
@@ -0,0 +1,4 @@
+# application insights key of aq77-appInsightshackfest for use by COSMOS DB
+d8008601-0c9c-4236-b9b2-b8eae1fae4aa
+
+export APPINSIGHTS_INSTRUMENTATIONKEY='d8008601-0c9c-4236-b9b2-b8eae1fae4aa'
